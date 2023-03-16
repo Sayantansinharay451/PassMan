8ae0731530ece20e3d75a808c4545eda08f5b80b
Sayantan Sinharay <s.sinharay.coder451@gmail.com>
2023-03-16 16:53:31 +0530

    feat : add all the necessary files

---
+++ passman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Purpose: A simple script to give a template for your shell script
+#Version: 1.0
+#Created Date: Tue Mar 14 03:58:48 PM IST 2023
+#Modified Date:
+#Author: Sayantan Sinharay
+# START #
+
+# END #
